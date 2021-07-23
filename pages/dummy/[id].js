1000b9d43553fd6a021fbdae44e1d402a180ef5d
lesronn <victoroforiacheampong@gmail.com>
2021-07-23 15:36:59 +0000

    revert changes back to the default

--- pages/dummy/[id].js
+++ pages/dummy/[id].js
@@ -1,36 +1,36 @@
-// export const getStaticPaths = async () => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const data = await res.json();
-//   const paths = data.map((dum) => {
-//     return {
-//       params: { id: dum.id.toString() },
-//     };
-//   });
-//   return {
-//     props: {
-//       paths,
-//       fallback: false,
-//     },
-//   };
-// };
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map((dum) => {
+    return {
+      params: { id: dum.id.toString() },
+    };
+  });
+  return {
+    props: {
+      paths,
+      fallback: false,
+    },
+  };
+};
 
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
-//   const data = await res.json();
-//   return {
-//     props: {
-//       dum: data,
-//     },
-//   };
-// };
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+  return {
+    props: {
+      dum: data,
+    },
+  };
+};
+const Details = (dum) => {
   return (
     <div>
-      {/* <h1>{dum.name}</h1>
+      <h1>{dum.name}</h1>
       <p>{dum.email}</p>
       <p>{dum.website}</p>
-      <p>{dum.address.city}</p> */}
+      <p>{dum.address.city}</p>
     </div>
   );
 };
